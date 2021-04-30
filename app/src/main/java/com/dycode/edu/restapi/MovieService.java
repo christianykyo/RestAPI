5dfa440b1e07a8e358ec07d851b1356a198bb7c2
christianykyo <christiany_kikyo_28rpl@student.smktelkom-mlg.sch.id>
2021-04-30 21:01:41 +0700

    commit

---
+++ app/src/main/java/com/dycode/edu/restapi/MovieService.java
@@ -0,0 +1,14 @@
+package com.dycode.edu.restapi;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface MovieService {
+
+//@GET("/JSONParsingTutorial/jsonMovie")
+//Call<MovieService> getMovies();
+
+    //merubah kode menjadi seperi berikut
+    @GET("/")
+    Call<MovieResponse> getMovies();
+}
